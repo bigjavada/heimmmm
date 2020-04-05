b3e58f0446f8854c276e431ca599fcd2b985c8ac
My Name <da@qq.com>
2020-04-05 20:41:30 +0800

    创建路由

--- src/main.js
+++ src/main.js
@@ -1,8 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
-
 Vue.config.productionTip = false
+
+// 导入子路由
+// import login from './components/login.vue';//登录子路由
+// 1:使用element-ui
+// 1.1：导包
+import ElementUI from 'element-ui';
+// 1.2：导入样式
+import 'element-ui/lib/theme-chalk/index.css';
+// 1.3：注册
+Vue.use(ElementUI);
 
 new Vue({
   render: h => h(App),
+  // 2.4:注册到实例中
+  router
 }).$mount('#app')
---
+++ src/router/router.js
@@ -0,0 +1,25 @@
+/* 
+1使用路由
+1.1:安装路由
+1.2:导入路由
+1.3:注册路由
+1.4:实例化
+1.5:注入路由
+1.6:在app.vue中写路由接口(router-view)
+*/
+// 1.0:导入子组件
+
+
+// 1.2：导入路由
+import VueRouter from 'vue-router';
+// 1.2:注册
+Vue.use(VueRouter);
+// 1.4:实例化
+const router = new VueRouter({
+  routes:[
+      //2.3.1默认路由
+      {path:"/", component:login }
+  ]
+});
+// 1.5:输出出去
+export default router;
